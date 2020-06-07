b691e88cb6e66096d38bd204acfa200283abc709
qwlake <qwlake@gmail.com>
2020-06-07 13:42:33 +0900

    feat: Django index app 추가

--- src/project/settings.py
+++ src/project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'index'
 ]
 
 MIDDLEWARE = [
@@ -123,3 +124,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, '.static_root')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'index'),
+]
